8d7512000e81d9e255cf5202f9fb1ac6c1984680
g le-chartier <g.le-chartier@ecam.fr>
2024-04-10 16:50:13 +0200

    merge

---
+++ src/backend/Table.java
@@ -0,0 +1,60 @@
+package backend;
+
+import java.util.ArrayList;
+
+public class Table {
+    private int height;
+    private int width;
+    private ArrayList<ArrayList<Cell>> table; 
+
+    //TODO-INPROGRESS : create constructor
+    public Table(int height, int width) {
+        this.height = height;
+        this.width = width;
+
+        //initialize the table
+        int vertexCount = 3;
+        table = new ArrayList<>(vertexCount);
+    }
+
+    public int getheight() {
+        return this.height;
+    }
+
+    public int getwidth() {
+        return this.width;
+    }
+
+    //TODO-COMPLETE : create getCell
+    public Cell getCell(int x,int y) {
+        //return the Cell object of coordinates x, y
+        return table.get(x).get(y);
+    }
+    //TODO-complete : set(Cell, x, y) set an object Cell to coordinate x, y
+    public void setCell(Cell cell, int x int y){
+        this.table.get(x).get(y) = cell;
+    }
+    //TODO-complete : count near (xy) -> return how many cells around this cell
+    public int countNear(int x, int y){
+        int cellCount =0;
+        // if border is true 
+        for (int i = x-1;i<=x+1;i++){
+            for (int j = y-1;j<=y+1;y++){
+                if not(i == j){
+                    this.table.getCell(i,j).getValue +=cellCount;
+                }
+            }
+            
+        }
+        
+        //if border is false
+
+    }
+    //TODO : set agent (x y agent) load an agent to coordinates x,y
+
+    //TODO : set random (density) create a random table of determined density 
+
+
+    //TODO : load(filepath) turn a loaded saveable file into a table
+    //TODO : save(filename) turn the table into saveable file
+}
