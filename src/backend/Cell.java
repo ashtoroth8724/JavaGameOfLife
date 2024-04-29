214eaffeeecc6688a2c10e5c5940e112eb3a012a
g le-chartier <g.le-chartier@ecam.fr>
2024-04-29 16:32:07 +0200

    Removed density from Cell

--- src/backend/Cell.java
+++ src/backend/Cell.java
@@ -2,27 +2,22 @@ package backend;
 
 public class Cell {
     private int value;
-	private int density;
 
-    public Cell(int value, int density) {
+
+    public Cell(int value) {
 		this.value = value;
-		this.density = density;
 	}
 
 	public int getValue() {
 		return value;
 	}
 	
-	public int getDensity() {
-		return density;
-	}
+	
 
 	public void setValue(int value) {
 		this.value = value;
 	}
 
-	public void setDensity(int density){
-		this.density = density;
-	}
+
 
 }
